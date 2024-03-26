8c5c38d12a4c3cb5092f28557189849bb72d49c8
Jackson Meyer <jmeyer@gssmail.com>
2024-03-26 16:32:46 -0500

    Added SQL file: GCG_7476_COMM_CODES.sql by Jackson Meyer

--- GCG_7476_COMM_CODES.sql
+++ GCG_7476_COMM_CODES.sql
@@ -1,4 +1,4 @@
 CREATE TABLE "GCG_7476_COMM_CODES" (
  "Commodity_Code" CHAR(20) NOT NULL,
- "Commodity_Desc" CHAR(100),
+ "Commodity_Desc" VARCHAR(100),
  "Inactive_Flag" CHAR(1) DEFAULT 'N' NOT NULL );
