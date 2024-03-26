ddb0b037f8528f6f44fa183e3e446e6b72b484aa
Jackson Meyer <jmeyer@gssmail.com>
2024-03-26 16:32:46 -0500

    Added SQL file: GCG_7476_QUAL_CODES.sql by Jackson Meyer

--- GCG_7476_QUAL_CODES.sql
+++ GCG_7476_QUAL_CODES.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "GCG_7476_QUAL_CODES" (
  "Record_Nbr" CHAR(3) NOT NULL,
  "Quality_Code" CHAR(2) NOT NULL,
- "Quality_Desc" CHAR(80),
+ "Quality_Desc" VARCHAR(80),
  "Inactive_Flag" CHAR(1) DEFAULT 'N' NOT NULL );
